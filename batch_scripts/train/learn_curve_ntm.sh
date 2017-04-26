c3c7b18ecfa182de53e478552eff28f4369e2835
Conor Sexton <sexton.conor@gmail.com>
2017-04-26 12:11:17 +0100

    Changed checkpoint save location in batch file

--- batch_scripts/train/learn_curve_ntm.sh
+++ batch_scripts/train/learn_curve_ntm.sh
@@ -1,20 +1,20 @@
 # 10k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 10000 --checkpoint_dir "checkpoint_lstm/learn_curve/10k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 10000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/10k"
 # 20k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 20000 --checkpoint_dir "checkpoint_lstm/learn_curve/20k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 20000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/20k"
 # 30k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 30000 --checkpoint_dir "checkpoint_lstm/learn_curve/30k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 30000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/30k"
 # 40k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 40000 --checkpoint_dir "checkpoint_lstm/learn_curve/40k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 40000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/40k"
 # 50k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 50000 --checkpoint_dir "checkpoint_lstm/learn_curve/50k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 50000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/50k"
 # 60k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 60000 --checkpoint_dir "checkpoint_lstm/learn_curve/60k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 60000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/60k"
 # 70k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 70000 --checkpoint_dir "checkpoint_lstm/learn_curve/70k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 70000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/70k"
 # 80k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 80000 --checkpoint_dir "checkpoint_lstm/learn_curve/80k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 80000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/80k"
 # 90k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 90000 --checkpoint_dir "checkpoint_lstm/learn_curve/90k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 90000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/90k"
 # 100k
-python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 100000 --checkpoint_dir "checkpoint_lstm/learn_curve/100k"
+python3 ~/Documents/Dissertation/main.py --is_LSTM_mode False --is_train True --continue_train False --train_set_size 100000 --checkpoint_dir "~/Documents/Dissertation/checkpoint_ntm/learn_curve/100k"

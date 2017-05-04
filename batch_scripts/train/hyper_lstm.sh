a6a8a75e85def51575302b6d41d5490736ab9eaf
Conor Sexton <sexton.conor@gmail.com>
2017-05-04 15:54:02 +0100

    and again

--- batch_scripts/train/hyper_lstm.sh
+++ batch_scripts/train/hyper_lstm.sh
@@ -1,4 +1,5 @@
 cd ../
+cd ../
 for ((i=$1; i <= $1 + 5; i++))
 do
     python3 main.py --is_LSTM_mode True --is_train True --rand_hyper True --continue_train False --train_set_size 40000 --checkpoint_dir "checkpoint_lstm/hyper/$i" --dataset_dir "dataset_files"

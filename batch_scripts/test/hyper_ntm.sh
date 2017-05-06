553ecfdf86da87005f3bace4669b88cc305e5c3f
Conor Sexton <sexton.conor@gmail.com>
2017-05-06 09:07:14 +0100

    change batch back to 6

--- batch_scripts/test/hyper_ntm.sh
+++ batch_scripts/test/hyper_ntm.sh
@@ -1,6 +1,6 @@
 cd ../
 cd ../
-for ((i=$1; i < $1 + 5; i++))
+for ((i=$1; i <= $1 + 5; i++))
 do
     python3 main.py --is_LSTM_mode False --is_test True --rand_hyper True --continue_train False --val_set_size 0 --checkpoint_dir "checkpoint_ntm/hyper/$i" --dataset_dir "dataset_files"
 done

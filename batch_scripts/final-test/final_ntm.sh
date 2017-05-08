eddc5256a8f30e06b508fe00194bf223ccb1d4cd
Conor Sexton <sexton.conor@gmail.com>
2017-05-08 23:37:14 +0100

    change directory

--- batch_scripts/final-test/final_ntm.sh
+++ batch_scripts/final-test/final_ntm.sh
@@ -1,3 +1,3 @@
 cd ../
 cd ../
-python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/16" --dataset_dir "dataset_files"
+python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/hyper/16" --dataset_dir "dataset_files"

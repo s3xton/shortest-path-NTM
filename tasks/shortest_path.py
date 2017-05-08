5634001801569cd31553a1e8f9c81acc879c9b9f
Conor Sexton <sexton.conor@gmail.com>
2017-05-09 00:17:01 +0100

    wrong order

--- tasks/shortest_path.py
+++ tasks/shortest_path.py
@@ -121,12 +121,13 @@ def run(ntm, config, sess):
 
     # Load the dataset from the file and get training sets
     dset = dataset.Dataset(config.graph_size, config.dataset_dir)
-    if config.is_test:
-        input_set, target_set, lengths, dist, unencoded = dset.get_validation_data(config.val_set_size)
-        print(dist)
-    elif config.is_final_test:
+    if config.is_final_test:
         input_set, target_set, lengths, dist, unencoded = dset.get_test_data(0)
         print(dist)
+    elif config.is_test:
+        input_set, target_set, lengths, dist, unencoded = dset.get_validation_data(config.val_set_size)
+        print(dist)
+    
 
     # 1) Completely wrong
     abs_error = []#[0] * config.graph_size - 1

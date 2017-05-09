d118f377fd69e1c827e18383c38a7f00757652df
Conor Sexton <sexton.conor@gmail.com>
2017-05-09 08:49:44 +0100

    wrong directory

--- batch_scripts/final-test/final_lstm.sh
+++ batch_scripts/final-test/final_lstm.sh
@@ -1,3 +1,3 @@
 cd ../
 cd ../
-python3 main.py --is_LSTM_mode True --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final/4" --dataset_dir "dataset_files"
+python3 main.py --is_LSTM_mode True --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_lstm/final/4" --dataset_dir "dataset_files"

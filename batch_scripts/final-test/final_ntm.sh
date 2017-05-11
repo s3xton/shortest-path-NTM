e80a80070dc66d63842438b15946014ceee8b98a
Conor Sexton <sexton.conor@gmail.com>
2017-05-11 19:56:39 +0100

    update final batch files

--- batch_scripts/final-test/final_ntm.sh
+++ batch_scripts/final-test/final_ntm.sh
@@ -1,6 +1,7 @@
 cd ../
 cd ../
-python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-7/16" --dataset_dir "larger_datasets/7-node"
-python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-8/16" --dataset_dir "larger_datasets/8-node"
-python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-9/16" --dataset_dir "larger_datasets/9-node"
-python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-10/16" --dataset_dir "larger_datasets/10-node"
+python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-6/69" --dataset_dir "larger_datasets/7-node"
+python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-7/69" --dataset_dir "larger_datasets/7-node"
+python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-8/69" --dataset_dir "larger_datasets/8-node"
+python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-9/69" --dataset_dir "larger_datasets/9-node"
+python3 main.py --is_LSTM_mode False --is_final_test True --is_test True --rand_hyper True --continue_train False --test_set_size 0 --checkpoint_dir "checkpoint_ntm/final-10/69" --dataset_dir "larger_datasets/10-node"
